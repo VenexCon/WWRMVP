ab8f17e44377c2663e8ba004cb0077209a0b899d
VenexCon <venexconsultant@outlook.com>
2023-02-17 13:50:55 +0000

    <Fix> Expresses catches errors after the routes due to atuo next pass

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,8 @@ const PORT = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 //connect to DB
-connectDB();
+//connectDB();
 const app = express();
-app.use(errorHandler);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +19,8 @@ app.get("/", (req, res) => {
 
 /* Routes */
 app.use("/users", require("./routes/userRoutes"));
+//errorhandler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
