b1174cc6b543d035ded78d453ae439c76fdfd24c
VenexCon <venexconsultant@outlook.com>
2023-02-23 10:48:21 +0100

    <FE Upd> inst packages, tailwindcss config & headless ui

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  return (
+    <>
+   Navbar here !
+    </>
+  )
+}
+
+export default Navbar
