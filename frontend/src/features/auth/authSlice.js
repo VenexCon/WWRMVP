582809b6a01a9c8fda7ec149bcb2c12e0621e578
venex <veenxconsultant@outlook.com>
2023-03-01 23:01:13 +0000

    <FE> began work on frontend for register user

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,49 +1,50 @@
-import { createSlice, createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  isRejectedWithValue,
+} from "@reduxjs/toolkit";
 import authService from "./authService";
 
-//allows us to use the redux Async life cycles with sync code. 
-import {extractErrorMessage} from '../../utils'
+//allows us to use the redux Async life cycles with sync code.
+import { extractErrorMessage } from "../../utils";
 
-const user = JSON.parse(localStorage.getItem('user'))
+const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
-  user: user ? user: null,
+  user: user ? user : null,
   isPending: false,
 };
 
 export const registerUser = createAsyncThunk(
-    "auth/register", 
-    async (user, thunkAPI) => {
-        try {
-            return await authService.register(user)
-        } catch (error) {
-            return thunkAPI(isRejectedWithValue(error))
-        }
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      return thunkAPI(isRejectedWithValue(error));
     }
-)
+  }
+);
 
 export const authSlice = createSlice({
-    name:'auth',
-    initialState, 
-    reducers : {
-        reset: (state) => {
-            state.isPending = false, 
-            state.isError = false, 
-            state.isSuccess = false, 
-            state.message = '',
-        },
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(register.pending, (state) => {
-            state.isPending = true; 
-        })
-        .addCase(register.fulfilled, (state) => {
-            state.isPending = false, 
-            state.user = action.payload
-        })
-        .addCase(register.rejected, (state) => {
-            state.isPending = false
-        })
-    }
-})
+  name: "auth",
+  initialState,
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isPending = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isPending = false;
+        state.user = action.payload;
+      })
+      .addCase(registerUser.rejected, (state) => {
+        state.isPending = false;
+      });
+  },
+});
+
+// we export the reducer function as this is what creates the
+// Reducer functions.
+export default authSlice.reducer;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,26 @@
-import {useState, useEffect, useRef} from 'react'
+import {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
 
 function Register() {
 
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.auth)
+
+  const registerData = useState({
+    email:'',
+    password:'',
+    Password2:'',
+    name:'',
+
+  })
 
   return (
-    <div></div>
+    <div>
+
+
+    </div>
   )
 }
 
