52889d7a0fc831d52fe4b7225b464909ed9dcbfd
venex <veenxconsultant@outlook.com>
2023-03-03 21:33:54 +0000

    <FE> register display created, func to be added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,22 @@ import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <div className="Container">
-        <Navbar />
-        <Routes>
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+      <Router>
+        <div className="Container">
+          <Navbar />
+          <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
 
-      <ToastContainer />
+        <ToastContainer />
+      </Router>
     </>
   );
 }
