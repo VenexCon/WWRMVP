25ae98a057d00d43fece9b89f4c67a07de4c689d
venex <venexconsultant@outlook.com>
2023-03-12 11:20:18 +0000

    <Business BE> business routes and middleare complete

---
+++ backend/middleware/businessAuthMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Business = require("../models/businessModel");
+
+const businessProtect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //get token from header
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //get user from token - remove password from returned object.
+      req.business = await Business.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error.message);
+      res.status(401);
+      throw new Error("Not authorized to view this page");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized to access this route");
+  }
+});
+
+module.exports = { businessProtect };
--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const { businessProtect } = require("../middleware/businessAuthMiddleware");
 const router = express.Router();
 const {
   registerBusiness,
   loginBusiness,
   getProfile,
+  deleteBusiness,
 } = require("../controllers/businessController");
 
 router.post("/", registerBusiness);
 router.post("/login", loginBusiness);
-router.get("/profile", getProfile);
+router.get("/profile", businessProtect, getProfile);
+router.delete("/profile", businessProtect, deleteBusiness);
 
 module.exports = router;
