c4c0f6a2ea3d5276da6c66a3b0ea942cb425f0f0
venex <venexconsultant@outlook.com>
2023-03-28 22:47:43 +0100

    <Upd> updated business and user profile with Link Btns

--- frontend/src/components/businessComponents/BusinessProfile.jsx
+++ frontend/src/components/businessComponents/BusinessProfile.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import {toast} from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { FaUserAlt, FaEnvelope, FaPhoneAlt, FaGlobe, FaWarehouse } from 'react-icons/fa'
+import { FaUserAlt, FaEnvelope, FaPhoneAlt, FaGlobe, FaWarehouse, FaPlusCircle, FaArrowCircleRight } from 'react-icons/fa'
 
 
 function BusinessProfile() {
@@ -11,7 +11,7 @@ function BusinessProfile() {
 
   return (
      <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8 mb-8">
         <div className="flex items-center space-x-2">
           <FaUserAlt className="text-gray-600" />
           <input
@@ -49,6 +49,18 @@ function BusinessProfile() {
           />
         </div>
       </div>
+      <div className="mt-10 flex flex-col space-y-4 w-full">
+          <Link to="/listings" className="w-full">
+            <button className="w-full flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-colors duration-300">
+              <FaArrowCircleRight className="mr-2" /> Browse Listings
+            </button>
+          </Link>
+          <Link to="/new-listing" className="w-full">
+            <button className="w-full flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 transition-colors duration-300">
+              <FaPlusCircle className="mr-2" /> Create New Listing
+            </button>
+          </Link>
+        </div>
     </>
   )
 }
