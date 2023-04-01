57da77c25fe0efb8c0e224000a4aec4c0244df8d
venex <venexconsultant@outlook.com>
2023-04-01 23:29:25 +0100

    <FE> listingitems in profile updated with link to listing item

--- frontend/src/components/sharedComponents/ListingItem.jsx
+++ frontend/src/components/sharedComponents/ListingItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ListingItem = ({ listing }) => {
   return (
-    <div className="bg-white shadow-md rounded-md p-4 mb-4">
-      <h2 className="text-lg font-semibold">{listing.listingTitle}</h2>
-      <p className="text-gray-500 text-sm my-2">{listing.listingDescription}</p>
-      <p className="text-gray-500 text-sm">{listing.listingLocation}</p>
+    
+    <div className="bg-gray-700 border border-purple-600 p-4 rounded-md">
+      <Link className='pointer-cursor ' to={`/listing/${listing._id}`} >
+      <h2 className="text-white text-lg font-bold mb-2">{listing.listingTitle}</h2>
+      <p className="text-white text-base">{listing.listingDescription}</p>
+      <p className="text-white text-sm mt-2">{listing.listingLocation}</p>
+      </Link>
     </div>
   );
 };
