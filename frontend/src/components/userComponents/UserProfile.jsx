63c4f442e7363efdeb420bb58c97eee6e111a2c0
venex <venexconsultant@outlook.com>
2023-04-19 23:01:57 +0100

    Removed unused state

--- frontend/src/components/userComponents/UserProfile.jsx
+++ frontend/src/components/userComponents/UserProfile.jsx
@@ -5,12 +5,23 @@ import { useSelector, useDispatch } from 'react-redux'
 import { FaUser, FaArrowCircleRight, FaEnvelope, FaPhone, FaLock,  } from 'react-icons/fa'
 
 function UserProfile() {
-    const {user} = useSelector((state) => state.auth)
+  
+  //to ensure no crafty states
+  useEffect(() => {
+    return () => {
+      setEdit(false)
+    }
+  }, [])
 
-      const [name, setName] = useState("John Doe");
-  const [email, setEmail] = useState("john.doe@example.com");
-  const [phone, setPhone] = useState("123-456-7890");
-  const [password, setPassword] = useState("");
+  const {user} = useSelector((state) => state.auth)
+  const [name, setName] = useState(user ? user.name : '');
+  const [email, setEmail] = useState(user? user.email: '');
+  //const [phone, setPhone] = useState(user ? user.phone: '');
+  const [edit, setEdit] =useState(false)
+
+  const selectEdit = (() => {
+    setEdit((prevState) => !prevState)
+  })
 
   return (
      <>
@@ -24,25 +35,26 @@ function UserProfile() {
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          disabled
-          className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-indigo-200 dark:bg-gray-700 dark:border-gray-700 dark:text-white"
-        />
+          disabled = {!edit}
+          className={`border rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-indigo-200 
+        ${edit ? 'bg-blue-700 border-blue-700 text-white' : 'dark:bg-gray-700 dark:border-gray-700 dark:text-white'}`}        />
       </div>
       <div className="flex flex-col space-y-2">
-        <div className="flex items-center space-x-2">
-          <FaEnvelope />
-          <label htmlFor="email">Email</label>
-        </div>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          disabled
-          className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-indigo-200 dark:bg-gray-700 dark:border-gray-700 dark:text-white"
-        />
+      <div className="flex items-center space-x-2">
+        <FaEnvelope />
+        <label htmlFor="email">Email</label>
       </div>
-      <div className="flex flex-col space-y-2">
+      <input
+        id="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={!edit}
+        className={`border rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-indigo-200 
+        ${edit ? 'bg-blue-700 border-blue-700 text-white' : 'dark:bg-gray-700 dark:border-gray-700 dark:text-white'}`}
+      />
+    </div>
+    {/*   <div className="flex flex-col space-y-2">
         <div className="flex items-center space-x-2">
           <FaPhone />
           <label htmlFor="phone">Phone</label>
@@ -52,10 +64,10 @@ function UserProfile() {
           type="tel"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
-          disabled
+          disabled ={!edit}
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-indigo-200 dark:bg-gray-700 dark:border-gray-700 dark:text-white"
         />
-      </div>
+      </div> */}
      {/*  <div className="flex flex-col space-y-2">
         <div className="flex items-center space-x-2">
           <FaLock />
@@ -71,11 +83,24 @@ function UserProfile() {
         />
       </div> */}
       <div className="mt-10 flex flex-col space-y-4 w-full">
-          <Link to="/listings" className="w-full">
-            <button className="w-full flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-colors duration-300">
-              <FaArrowCircleRight className="mr-2" /> Browse Listings
-            </button>
-          </Link>
+        {!edit && (
+          <button onClick={selectEdit}
+          className="w-full flex items-center justify-center bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-colors duration-300">
+            <FaUser className="mr-2" /> Edit Profile
+          </button>
+        )}
+        {edit && (
+          <button onClick={selectEdit}
+           className="w-full flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-colors duration-300">
+          <FaLock className="mr-2" /> Confirm Profile
+        </button>
+        )}
+        {!edit && (
+          <Link to="/listing" className="w-full">
+          <button className="w-full flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-colors duration-300">
+            <FaArrowCircleRight className="mr-2" /> Browse Listings
+          </button>
+        </Link>)}
         </div>
     </>
   )
