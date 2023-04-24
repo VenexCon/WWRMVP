9f81b788f2fdc93765926cc06ba06fb2a0d7ff38
venex <venexconsultant@outlook.com>
2023-04-24 21:40:55 +0100

    Update passwordReset

--- backend/controllers/passwordResetController.js
+++ backend/controllers/passwordResetController.js
@@ -12,7 +12,7 @@ sgMail.setApiKey(process.env.FORGOT_PASSWORD_KEY);
 //Check if user or business exists first.
 //change to return the actual account details minus the password
 const checkIfUserOrBusinessExists = async (email) => {
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: email });
   if (user) {
     return { type: "user", account: user };
   }
@@ -30,23 +30,23 @@ const resetPassword = asyncHandler(async (req, res) => {
 
   //Check the accounts exists and return false if not true.
   const account = await checkIfUserOrBusinessExists(email);
-  if (!account) {
+  if (account === false) {
     return res.status(404).json({ message: "Account not found" });
   }
   //generate a token to be used
-  const genToken = jwt.sign({ email: email }, process.env.RESET_PASSWORD_KEY, {
+  const token = jwt.sign({ email: email }, process.env.JWT_SECRET, {
     expiresIn: "20m",
   });
 
-  //Attach token to model
+  //Attach token to model & send token, do not call gen token here as it will result in two different tokens.
   if (account.type === "user") {
-    User.resetPasswordToken = genToken();
-    User.resetPasswordExpires = Date.now() + 600000; // 10 minutes
-    await user.save();
-  } else if (account.type === "business") {
-    Business.resetPasswordToken = genToken();
-    Business.resetPasswordExpires = Date.now() + 600000; // 10 minutes
-    await business.save();
+    account.account.resetToken = token;
+    account.account.resetTokenExpiration = Date.now() + 3600000; // 1 hour
+    await account.account.save();
+  } else {
+    account.account.businessResetToken = token;
+    account.account.businessResetTokenExpiration = Date.now() + 3600000; // 1 hour
+    await account.account.save();
   }
 
   //msg
@@ -58,7 +58,7 @@ const resetPassword = asyncHandler(async (req, res) => {
       "We received a request to reset your password. Please click the following link to reset your password: http://localhost:3000/reset-password/" +
       token,
     html: `<p>We received a request to reset your password. Please click the following link to reset your password: <a href="http://localhost:3000/reset-password/${token}">Reset Password</a></p>`,
-    reply_to: "",
+    reply_to: "admin@whowantsrubbish.com",
   };
 
   try {
