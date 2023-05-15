fedc60051bb182989a728718bd6f89265bb32d01
venex <venexconsultant@outlook.com>
2023-05-15 22:28:57 +0100

    Updated Home page styling

--- frontend/src/components/sharedComponents/PricingCard.jsx
+++ frontend/src/components/sharedComponents/PricingCard.jsx
@@ -13,10 +13,10 @@ function PricingCard() {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">£7.50</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-lg mb-4 font-semibold text-gray-700 mr-2 mb-2">£7.50</span>
   </div>
-  <Link to={'/register'} className='w-4/5 mb-3 mx-auto flex items-center justify-center gap-x-2 text-white bg-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
-    <button className="w-1/3 flex items-center align-middle justify-center text-blue-600 font-bold gap-x-2">
+  <Link to={'/register'} className='w-3/5 mb-3 mx-auto flex items-center justify-center gap-x-2 text-white bg-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
+    <button className="w-2/3 flex items-center align-middle justify-center text-blue-600 font-bold gap-x-2">
     <p>
         Sign-Up
     </p>
