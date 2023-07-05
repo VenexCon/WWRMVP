5fff1dd9636e9968971572e7327179d6e0c78a7e
venex <venexconsultant@outlook.com>
2023-07-05 22:26:55 +0100

    updated PricingCard

--- frontend/src/components/sharedComponents/PricingCard.jsx
+++ frontend/src/components/sharedComponents/PricingCard.jsx
@@ -4,7 +4,7 @@ import { FaCheck } from 'react-icons/fa';
 
 function PricingCard({ title, targetAudience, price, description }) {
   return (
-    <div className="max-w-md rounded-lg md:w-2/3 h-90 overflow-hidden shadow-lg bg-blue-700 text-white border border-black">
+    <div className="max-w-md rounded-lg md:w-2/3 h-90 overflow-hidden shadow-lg bg-blue-700 text-white border border-black ">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-base text-white mb-4">{targetAudience}</p>
@@ -24,9 +24,9 @@ function PricingCard({ title, targetAudience, price, description }) {
       </div>
       <Link
         to="/register"
-        className="w-3/5 mb-3 mx-auto flex items-center justify-center gap-x-2 text-blue-600 bg-white font-medium rounded-lg text-lg x-5 py-2.5 text-center hover:border hover:border-black"
+        className="w-3/5 mb-3 mx-auto flex items-center justify-center gap-x-2 text-blue-600 hover:text-white hover:bg-blue-400  bg-white font-medium rounded-lg text-lg x-5 py-2.5 text-center hover:scale-110 ease-in-out duration-300"
       >
-        <button className="w-2/3 flex items-center align-middle justify-center font-bold gap-x-2">
+        <button className="w-2/3 flex items-center align-middle justify-center font-bold gap-x-2 ">
           <p>Sign-up!</p>
         </button>
       </Link>
