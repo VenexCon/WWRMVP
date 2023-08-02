df53abe78ed30fea9c39775a79be30e809f0e705
venex <venexconsultant@outlook.com>
2023-08-02 21:45:17 +0100

    updated pricing Modal

--- frontend/src/components/sharedComponents/PriceModal.jsx
+++ frontend/src/components/sharedComponents/PriceModal.jsx
@@ -6,18 +6,19 @@ const PriceModal = ({ isOpen, onClose, onConfirmDelete }) => {
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onClose}
-      contentLabel="Delete Account Modal"
+      contentLabel="Subscription Selection Modal"
       className="fixed inset-0 flex items-center justify-center"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50"
     >
       <div className="bg-white rounded-lg p-6 max-w-md mx-auto">
-        <h2 className="text-xl font-bold mb-4">Confirm Delete</h2>
+        <h2 className="text-xl font-bold mb-4">Confirm Plan Type</h2>
         <p className="text-gray-600 mb-6">
-          Are you sure you want to delete your account? This action cannot be undone.
+          Please select from two available plans below.
         </p>
-        <p className="text-gray-600 mb-6">
-          Once deleted all listings you have created, or own will also be destroyed.
+        <p className="text-gray-600 mb-2">
+          Each plan has its own benefits so please select carefully.
         </p>
+        <p className="text-gray-600 mb-2">Each plan will be billed monthly</p>
         <div className="flex justify-between">
           <button
             className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md mr-2"
